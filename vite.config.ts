63a3a65efa5efbab9d9fc529a9ba9489de9bf3ee
Kylian Bardini <kylianb1@icloud.com>
2023-05-05 14:51:32 +0200

    🔧 config: add alias on path dir

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/',
+      '@app': '/src',
+      '@components': '/src/components',
+      '@pages': '/src/pages',
+      '@styles': '/src/styles',
+      '@utils': '/src/utils',
+      '@hooks': '/src/hooks',
+      '@assets': '/src/assets',
+      '@store': '/src/store',
+      '@interfaces': '/src/interfaces',
+      '@services': '/src/services',
+      '@public': '/public',
+    }
+  },
 })
